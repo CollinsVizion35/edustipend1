fa230ed2a5d5ea1e499f46b1cdbd8b1911e50aee
Okechukwu Collins Vizion <106563563+CollinsVizion35@users.noreply.github.com>
2023-05-18 13:39:37 +0000

    Update hero.js and sidebar.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-start space-y-2 w-4/5 pl-[10vw] h-fit">
-      <div className="text-4xl font-bold">Make your party fun!</div>
-      <div className="">Create your own custom playlist today.</div>
-      <button className="bg-[#FF9900] p-1 border-[1px] rounded-[5px] border-black mt-2">
+    <div className="flex flex-col items-start space-y-12 w-4/5 pl-[10vw] h-fit">
+      <div className="flex flex-col items-start">
+        <div className="text-[4vw] font-bold">Make your party fun!</div>
+        <div className="text-[1.5vw]">
+          Create your own custom playlist today.
+        </div>
+      </div>
+      <button className="bg-[#FF9900] p-2 text-[1vw] border-[1px] rounded-[5px] border-black  mt-1">
         Create playlist
       </button>
     </div>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,29 +1,161 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Sidebar = () => {
+  const audioRef = useRef(null);
+  const audioRef2 = useRef(null);
+  const audioRef3 = useRef(null);
+  const audioRef4 = useRef(null);
+  const audioRef5 = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+      audioRef2.current.pause();
+      audioRef3.current.pause();
+      audioRef4.current.pause();
+      audioRef5.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handlePlayPause2 = () => {
+    if (isPlaying) {
+      audioRef2.current.pause();
+    } else {
+      audioRef2.current.play();
+      audioRef.current.pause();
+      audioRef3.current.pause();
+      audioRef4.current.pause();
+      audioRef5.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handlePlayPause3 = () => {
+    if (isPlaying) {
+      audioRef3.current.pause();
+    } else {
+      audioRef3.current.play();
+      audioRef2.current.pause();
+      audioRef.current.pause();
+      audioRef4.current.pause();
+      audioRef5.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handlePlayPause4 = () => {
+    if (isPlaying) {
+      audioRef4.current.pause();
+    } else {
+      audioRef4.current.play();
+      audioRef2.current.pause();
+      audioRef3.current.pause();
+      audioRef.current.pause();
+      audioRef5.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handlePlayPause5 = () => {
+    if (isPlaying) {
+      audioRef5.current.pause();
+    } else {
+      audioRef5.current.play();
+      audioRef2.current.pause();
+      audioRef3.current.pause();
+      audioRef4.current.pause();
+      audioRef.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    <div className="w-1/5 bg-[#666666] flex flex-col items-center space-y-4 h-[75vh] w-fit p-4">
+    <div className="w-[20vw] bg-[#666666] text-[1.3vw] flex flex-col items-center space-y-7 h-[75%] mt-[10vh] w-fit p-4 mr-16">
       <header className="font-bold">Trending songs</header>
 
-      <div className="flex flex-col space-y-4">
-        <button className="border-[#666666] rounded-[5px] bg-[#FFFFFF] border p-2">
-          Unavailable by Davido +
-        </button>
-        <button className="border-[#666666] rounded-[5px] bg-[#FFFFFF] border p-2">
-          Unavailable by Davido +
-        </button>
-        <button className="border-[#666666] rounded-[5px] bg-[#FFFFFF] border p-2">
-          Unavailable by Davido +
-        </button>
-        <button className="border-[#666666] rounded-[5px] bg-[#FFFFFF] border p-2">
-          Unavailable by Davido +
-        </button>
-        <button className="border-[#666666] rounded-[5px] bg-[#FFFFFF] border p-2">
-          Unavailable by Davido +
-        </button>
+      <div className="flex flex-col space-y-6">
+        <div className="bg-white p-2 rounded-[5px] border-[#666666]">
+          <audio
+            ref={audioRef}
+            src="https://www.six9ja.com/wp-content/uploads/2023/03/Davido_Ft_Musa_Keys_-_Unavailable_Six9ja.mp3"
+          />
+          <button
+            onClick={handlePlayPause}
+            className="flex flex-row space-x-1 items-center"
+          >
+            <div>
+              Unavailable by <i>Davido</i>
+            </div>
+            <div className="font-bold flex flex-start">+</div>
+          </button>
+        </div>
+        <div className="bg-white p-2 rounded-[5px] border-[#666666]">
+          <audio
+            ref={audioRef2}
+            src="https://www.six9ja.com/wp-content/uploads/2023/03/Davido_Ft_Musa_Keys_-_Unavailable_Six9ja.mp3"
+          />
+          <button
+            onClick={handlePlayPause2}
+            className="flex flex-row space-x-1 items-center"
+          >
+            <div>
+              Unavailable by <i>Davido</i>
+            </div>
+            <div className="font-bold flex flex-start">+</div>
+          </button>
+        </div>
+        <div className="bg-white p-2 rounded-[5px] border-[#666666]">
+          <audio
+            ref={audioRef3}
+            src="https://www.six9ja.com/wp-content/uploads/2023/03/Davido_Ft_Musa_Keys_-_Unavailable_Six9ja.mp3"
+          />
+          <button
+            onClick={handlePlayPause3}
+            className="flex flex-row space-x-1 items-center"
+          >
+            <div>
+              Unavailable by <i>Davido</i>
+            </div>
+            <div className="font-bold flex flex-start">+</div>
+          </button>
+        </div>
+        <div className="bg-white p-2 rounded-[5px] border-[#666666]">
+          <audio
+            ref={audioRef4}
+            src="https://www.six9ja.com/wp-content/uploads/2023/03/Davido_Ft_Musa_Keys_-_Unavailable_Six9ja.mp3"
+          />
+          <button
+            onClick={handlePlayPause4}
+            className="flex flex-row space-x-1 items-center"
+          >
+            <div>
+              Unavailable by <i>Davido</i>
+            </div>
+            <div className="font-bold flex flex-start">+</div>
+          </button>
+        </div>
+        <div className="bg-white p-2 rounded-[5px] border-[#666666]">
+          <audio
+            ref={audioRef5}
+            src="https://www.six9ja.com/wp-content/uploads/2023/03/Davido_Ft_Musa_Keys_-_Unavailable_Six9ja.mp3"
+          />
+          <button
+            onClick={handlePlayPause5}
+            className="flex flex-row space-x-1 items-center"
+          >
+            <div>
+              Unavailable by <i>Davido</i>
+            </div>
+            <div className="font-bold flex flex-start">+</div>
+          </button>
+        </div>
       </div>
 
-      <button className="bg-[#FF9900] w-fit p-1 border-[1px] rounded-[5px] border-[#666666]">
+      <button className="bg-[#FF9900] w-fit p-2 border-[1px] rounded-[5px] border-[#666666]">
         View more
       </button>
     </div>
