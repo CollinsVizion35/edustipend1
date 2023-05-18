cebeecab80626171dc952558da3a694c2c677a9a
Okechukwu Collins Vizion <106563563+CollinsVizion35@users.noreply.github.com>
2023-05-18 12:58:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import React from "react";
+import Hero from "./components/hero";
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="">Make your party fun!</div>
+      <div className="">Create your own custom playlist today.</div>
+      <button className="">Create playlist</button>
+    </div>
+  );
+};
+
+export default Hero;
